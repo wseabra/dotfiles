c16d082052ec2b2d1377510df54d57c764aa5f6e
Waldomiro <wjuan.06@gmail.com>
2019-03-10 02:04:05 -0400

    creating install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Neovim
+mkdir $HOME/.config/nvim
+
+ln -s $HOME/dotfiles/nvim/init.vim $HOME/.config/nvim/
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#zsh
+ln -s $HOME/dotfiles/.zshrc $HOME/
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
