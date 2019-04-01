1d989a70409ce6b1b78f1338bfba09416dd7cc84
Waldomiro <wjuan.06@gmail.com>
2019-03-31 21:46:54 -0400

    moving .zshrc to zshrc and updating intall.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
+#Vim
+if [[ ! -d "$HOME/.vim" ]]; then
+    mkdir $HOME/.vim
+fi
+
+ln -s $HOME/dotfiles/vimrc $HOME/.vimrc
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 #Neovim
 if [[ ! -d "$HOME/.config/nvim" ]]; then
     mkdir $HOME/.config/nvim
 fi
 
-ln -s $HOME/dotfiles/nvim/init.vim $HOME/.config/nvim/
+ln -s $HOME/dotfiles/vimrc $HOME/.config/nvim/init.vim
 
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 #zsh
-ln -s $HOME/dotfiles/.zshrc $HOME/
+ln -s $HOME/dotfiles/zshrc $HOME/.zshrc
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
