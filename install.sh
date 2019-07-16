65d373b6e64304c78ad9782817f3bc2e353b4caf
Waldomiro Seabra <wseabra@gitlab.indt.org.br>
2019-07-16 11:00:19 -0400

    replacing $HOME/dotfiles to $PWD

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 #Tmux
-ln -sf $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
-ln -sf $HOME/dotfiles/simple.conf $HOME/.simple.conf
+ln -sf $PWD/tmux.conf $HOME/.tmux.conf
+
+ln -sf $PWD/simple.conf $HOME/.simple.conf
 
 #Vim
 mkdir -p $HOME/.vim
 
-ln -sf $HOME/dotfiles/vimrc $HOME/.vimrc
+ln -sf $PWD/vimrc $HOME/.vimrc
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 #zsh
-ln -sf $HOME/dotfiles/zshrc $HOME/.zshrc
+ln -sf $PWD/zshrc $HOME/.zshrc
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
