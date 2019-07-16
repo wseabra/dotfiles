014ae2de4bb0968d31c09df5754632bb56ff2486
Waldomiro Seabra <wseabra@gitlab.indt.org.br>
2019-07-16 11:08:52 -0400

    echoing progress in install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 #Tmux
+echo "-----Installing tmux configurations-----"
 ln -sf $PWD/tmux.conf $HOME/.tmux.conf
 
 ln -sf $PWD/simple.conf $HOME/.simple.conf
 
+echo "-----tmux configurations installed-----"
 #Vim
+echo "-----Installing vim configurations-----"
 mkdir -p $HOME/.vim
 
 ln -sf $PWD/vimrc $HOME/.vimrc
@@ -13,8 +16,10 @@ ln -sf $PWD/vimrc $HOME/.vimrc
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+echo "-----vim configurations installed-----"
 #zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+echo "-----Installing zsh configurations-----"
+sh -c "RUNZSH=no $(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions \
     ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -23,4 +28,5 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
     ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 ln -sf $PWD/zshrc $HOME/.zshrc
+echo "-----zsh configurations installed-----"
 
