e26087100560980f79ba304c538c10b0ca939ca2
Waldomiro Seabra <wjuan.06@gmail.com>
2019-07-17 21:24:46 -0400

    adding my awesome setup

---
+++ awesome/autorun.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+function run {
+  if ! pgrep -f $1 ;
+  then
+    $@&
+  fi
+}
+
+# run program [some arguments]
+run redshift-gtk&
+run compton&
+run nm-applet&
+run pamac-tray&
+run blueman-applet&
+run nitrogen --restore
+run insync start
+run dropbox start
+run light-locker --lock-on-suspend&
