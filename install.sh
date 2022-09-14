19073dd87f9ba3915e4c91602a6b74385233b914
Waldomiro Seabra <waldomiro.seabra@stone.com.br>
2022-09-14 16:11:55 -0400

    adding new install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s $PWD/zshrc $HOME/.zshrc
+ln -s $PWD/vimrc $HOME/.vimrc
+ln -s $PWD/herbstluftwm/ $HOME/.config/herbstluftwm
+ln -s $PWD/polybar/ $HOME/.config/polybar
+ln -s $PWD/screenlayout/ $HOME/.screenlayout/
