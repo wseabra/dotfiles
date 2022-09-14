faf22cdd47f303b02c4d062fdc7e75057d0de176
Waldomiro Seabra <waldomiro.seabra@stone.com.br>
2022-09-14 16:06:36 -0400

    adding monitor_layout.sh

--- switch_monitor_layout.sh
+++ switch_monitor_layout.sh
@@ -6,10 +6,10 @@ if [ $MonitorCount -eq 3 ]; then
     "$OfficeSetupScript" &
     if [ $VirtualMonitorCount -eq 4 ]; then
         notify-send --icon=gtk-info Monitors "3 monitors found\nChanging to office setup"
-        herbstclient set_monitors 1920x1080+0+560 2560x1080+1920+560 1080x1920+4480+0
+        herbstclient set_monitors 1920x1080+0+560 2560x1032+1920+560 1080x1920+4480+0
     else
         notify-send --icon=gtk-info Monitors "3 monitors found\nChanging to office setup (1 Virtual Monitor)"
-        herbstclient set_monitors 1920x1080+0+560 2100x1080+1920+560 460x1080+4020+560 1080x1920+4480+0
+        herbstclient set_monitors 1920x1080+0+560 2100x1032+1920+560 460x1032+4020+560 1080x1920+4480+0
     fi
 else
 # or simply:
