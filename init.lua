56df6230a96b2074c6da08d77be630bae6b1185c
Waldomiro Seabra <waldomiro.seabra@stone.com.br>
2022-12-28 14:54:41 -0400

    updating init.lua

--- init.lua
+++ init.lua
@@ -23,12 +23,23 @@ require('packer').startup(function(use)
 
     use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
 
+    use 'tpope/vim-sleuth'
+
+    -- Git
     use 'tpope/vim-fugitive'
 
+    use 'lewis6991/gitsigns.nvim'
+
+    -- Themes
     use 'tomasr/molokai'
 
     use 'ayu-theme/ayu-vim'
 
+    use 'lifepillar/vim-solarized8'
+
+    use {'dracula/vim', as = 'dracula'}
+
+    -- Statusline/tabline
     use {
             'nvim-lualine/lualine.nvim',
             requires = { 'kyazdani42/nvim-web-devicons', opt = true }
@@ -40,8 +51,11 @@ require('packer').startup(function(use)
         plugins(use)
     end
 
+    -- if is bootstrap sync everything otherwise just download new plugins
     if is_bootstrap then
         require('packer').sync()
+    else
+        require('packer').install()
     end
 end)
 
@@ -84,18 +98,20 @@ vim.api.nvim_create_autocmd('BufEnter',
     pattern = 'init.lua,.vimrc',
     command = [[
     setlocal foldmethod=marker
-    ]]})
+    ]]
+})
 vim.api.nvim_create_autocmd('BufRead', 
 {
     pattern = 'init.lua,.vimrc',
     command = [[
     :normal zM
-    ]]})
+    ]]
+})
 
 -- May remove with tpope/vim-sleuth
-vim.opt.expandtab = true -- expand tab into spaces
-vim.opt.shiftwidth=4 -- size of identation
-vim.opt.tabstop=4 -- size of tab
+-- vim.opt.expandtab = true -- expand tab into spaces
+-- vim.opt.shiftwidth=4 -- size of identation
+-- vim.opt.tabstop=4 -- size of tab
 
 vim.opt.list=true
 vim.opt.listchars = {eol = '¬', trail = '·'} --show end of line and trailing whitespaces/tabs
@@ -136,23 +152,23 @@ require('lualine').setup {
         lualine_y = {},
         lualine_z = {}
     },
-        tabline = {
-                lualine_a = {
-                        {
-                        'buffers',
-                        mode = 4,
-                        }
-                },
-                lualine_b = {},
-                lualine_c = {},
-                lualine_x = {},
-                lualine_y = {},
-                lualine_z = {'tabs'}
+    tabline = {
+        lualine_a = {
+            {
+                'buffers',
+                mode = 4,
+            }
         },
-        options = {
-                component_separators = '|',
-                section_separators = '',
-        },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = {'tabs'}
+    },
+    options = {
+        component_separators = '|',
+        section_separators = '',
+    },
 }
 ---}}}
 
@@ -160,13 +176,17 @@ require('lualine').setup {
 require('Comment').setup()
 --}}}
 
+--{{{Gitsigns
+require('gitsigns').setup()
+--}}}
+
 --{{{Local functions for keymaps
 local SpellToggle = function()
-        if vim.opt.spell:get() then
-                vim.opt.spell=false
-        else
-                vim.opt.spell=true
-        end
+    if vim.opt.spell:get() then
+        vim.opt.spell=false
+    else
+        vim.opt.spell=true
+    end
 end
 
 local RltvNrToggle = function()
